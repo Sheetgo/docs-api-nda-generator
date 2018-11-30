b7c9a6fdd9a41ef759a5aa06044ee0883b9d97e2
Fernando Cacenot <fernando@sheetgo.com>
2018-11-30 13:52:15 -0200

    feat: Set link and status of nda when is generated

--- src/Drive.js
+++ src/Drive.js
@@ -66,3 +66,34 @@ function saveBlobAsFile(blob, name, folder) {
     var id = file.getId()
     return [url, id]
 }
+
+/**
+ * Returns repository folder and create if not exists
+ */
+function getRepositoryFolder() {
+    // Get NDA doc name from settings sheet
+    var repositoryFolderName = SpreadsheetApp.getActive()
+        .getSheetByName('Settings')
+        .getRange('C8')
+        .getValue()
+
+    var repositoryFolder = null
+    var parentFolderId = DriveApp.getFolderById(SpreadsheetApp.getActive().getId()).getParents().next().getId()
+    var childrenFolders = DriveApp.getFolderById(parentFolderId).getFolders()
+
+    // Checks if nda folder exists
+    while (childrenFolders.hasNext()) {
+        var childfolder = childrenFolders.next()
+        if (childfolder.getName() === repositoryFolderName) {
+            repositoryFolder = childfolder
+        }
+    }
+
+    // Create nda folder if not exists
+    if (!repositoryFolder) {
+        repositoryFolder = DriveApp.getFolderById(parentFolderId).createFolder(repositoryFolderName)
+    }
+
+    return repositoryFolder
+}
+
